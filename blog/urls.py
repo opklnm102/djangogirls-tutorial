68a0e6affcc301bdd345b949a9bf742937569ed6
DongHee Kim <opklnm102@gmail.com>
2017-03-02 20:14:18 +0900

    Add: Added views to create/edit blog post inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     # 문자열이 아무것도 없는 경우 매칭
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail')
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
