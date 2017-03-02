abe55a3ed6d563144fd4750622c10dbd38645049
DongHee Kim <opklnm102@gmail.com>
2017-03-02 18:58:53 +0900

    Add: view and template for detailed blog post as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # 문자열이 아무것도 없는 경우 매칭
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail')
 ]
